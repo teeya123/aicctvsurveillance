c76866e9f9e729f0e27705ced1424105cafbe58a
teeya123 <73390143+teeya123@users.noreply.github.com>
2021-06-02 18:43:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 video="";
 status="";
-
+objects=[];
 function preload(){
     video=createVideo('video.mp4');
     video.hide();
@@ -11,7 +11,27 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+    if(status !=""){
+        objectDetector.detect(video,gotResults);
+        for (i=0;i<objects.lenght;i++) {
+            document.getElementById("status").innerHTML="Status:Objects Detected";
+            document.getElementById("number_of_objects").innerHTML="Number of objects"+objects.length;
+            fill ("#FF0000");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y);
+            noFill();
+stroke("#FF0000");
+rect (objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
